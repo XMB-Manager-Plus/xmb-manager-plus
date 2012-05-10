eaa0876a4fee7406cfbda11c40bec6ebc7102a10
andreus sebes <andreus.sebes@gmail.com>
2012-05-10 01:23:47 +0100

    resolved some bug encontered by xioron

--- bin/global_prechecks.bat
+++ bin/global_prechecks.bat
@@ -15,6 +15,6 @@ reg add HKEY_CURRENT_USER\Console /v FontFamily /t REG_DWORD /d 00000036 /f
 reg add HKEY_CURRENT_USER\Console /v FontSize /t REG_DWORD /d 00786432 /f
 reg add HKEY_CURRENT_USER\Console /v FontWeight /t REG_DWORD /d 00000190 /f
 %external%\ssr\ssr --nobackup --recurse --encoding ansi --dir "%bindir%" --include "settings.ini" --alter --search "encoding_prep=no" --replace "encoding_prep=yes"
-start %~1
+start "" "%~1"
 exit
 )
